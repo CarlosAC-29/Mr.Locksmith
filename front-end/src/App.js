fbce10812d68501a0c91cd8d897f92ce4528b4b1
valenlzmjAP <valentina.loaiza.mejia2001@gmail.com>
2022-09-05 18:46:14 -0500

    footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Footer />
     </div>
   );
 }
