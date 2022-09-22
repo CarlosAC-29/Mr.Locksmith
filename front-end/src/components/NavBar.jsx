31ac8ed78000649efeeed7838aa25f2657a4127d
Carlos Cáceres <carlosmicro33@gmail.com>
2022-09-21 20:34:48 -0500

    Responsive navbar

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -7,36 +7,39 @@ import { AiOutlineClose } from 'react-icons/ai';
 
 export default function Navbar() {
 
-    const [isMobile, setIsMobile] = useState(false);
+	const [isMobile, setIsMobile] = useState(false);
 
-    return (
-        <div className='navbar'>
-            <img className='navbar__logo' src={logo} alt='Mr. locksmith logo' />
-            <div>
-                <nav className={isMobile ? 'navbar__mobile' : 'navbar__links'} onClick={() => setIsMobile(false)}>
-                    <CustomLink to="/">Home</CustomLink>
-                    <CustomLink to="/services">Services</CustomLink>
-                    <CustomLink to="/locations">Locations</CustomLink>
-                    <CustomLink to="/contact-us">Contact us</CustomLink>
-                </nav>
-            </div>
-            <div className='container__toggler'>
-                <button className='btn__toggler' onClick={() => setIsMobile(!isMobile)}>
-                    {isMobile ? <AiOutlineClose /> : <BiMenuAltRight />}
-                </button>
-            </div>
-        </div>
-    )
+	return (
+		<div className='navbar'>
+			<img className='navbar__logo' src={logo} alt='Mr. locksmith logo' />
+			<div>
+				<nav className={isMobile ? 'navbar__mobile' : 'navbar__links'} onClick={() => setIsMobile(false)}>
+					<button className='btn__toggler__close' onClick={() => setIsMobile(!isMobile)}>
+						<AiOutlineClose />
+					</button>
+					<CustomLink to="/">Home</CustomLink>
+					<CustomLink to="/services">Services</CustomLink>
+					<CustomLink to="/locations">Locations</CustomLink>
+					<CustomLink to="/contact-us">Contact us</CustomLink>
+				</nav>
+			</div>
+			<div className='container__toggler'>
+				<button className='btn__toggler' onClick={() => setIsMobile(!isMobile)}>
+					<BiMenuAltRight />
+				</button>
+			</div>
+		</div>
+	)
 }
 
-function CustomLink({ to, children}) {
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-    const navigate = useNavigate();
+function CustomLink({ to, children }) {
+	const resolvedPath = useResolvedPath(to)
+	const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+	const navigate = useNavigate();
 
-    return (
-        <button  className={isActive ? "btn__page active" : "btn__page"} onClick={() => navigate(to)}>
-            {children}
-        </button>
-    )
+	return (
+		<button className={isActive ? "btn__page active" : "btn__page"} onClick={() => navigate(to)}>
+			{children}
+		</button>
+	)
 }
